681745d8cf6d08164a0d2acb68f5a0cae220e9ba
chutified <tommychu2256@gmail.com>
2021-01-29 15:53:05 +0100

    Add getAccountByID HTTP handler

--- api/account.go
+++ api/account.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/chutified/simple-bank/db/sqlc"
@@ -31,6 +33,33 @@ func (s *Server) createAccount(c *gin.Context) {
 	account, err := s.store.CreateAccount(c, params)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
+
+		return
+	}
+
+	c.JSON(http.StatusOK, account)
+}
+
+type getAccountByIDRequest struct {
+	ID int64 `uri:"id" binding:"required,numeric,min=1"`
+}
+
+func (s *Server) getAccountByID(c *gin.Context) {
+	var req getAccountByIDRequest
+	if err := c.ShouldBindUri(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+
+		return
+	}
+
+	// query the account ID
+	account, err := s.store.GetAccount(c, req.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			c.JSON(http.StatusNotFound, errorResponse(err))
+		} else {
+			c.JSON(http.StatusInternalServerError, errorResponse(err))
+		}
 
 		return
 	}
--- api/routing.go
+++ api/routing.go
@@ -4,11 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// getRouter sets up the routing for the given Server and returns
+// the constructed gin router.
 func getRouter(s *Server) *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
 	r.POST("/accounts", s.createAccount)
+	r.GET("/accounts/:id", s.getAccountByID)
 
 	return r
 }
